4874b7b5aeaaf46be72f1f454083109007ebe189
mylliejohnson <mylliejohnson@gmail.com>
2021-08-03 19:06:13 -0400

    bootstrap rows and columns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {Button, Container, Row, Col} from 'react-bootstrap/'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="App" className="container-fluid">
+   
+    <h1 className="text-center">hello</h1>
+      <Button>hello</Button>
+      
+      <Container style={{border:'1px solid black'}}>
+<div>
+      <div className="text-center row">
+        <Col>Hello</Col>
+        <Col>No</Col>
+        <Col>Yes</Col>
+        <Col>Goodbye</Col>
+      </div>
+      <div className="text-center row">
+        <Col className='col-7'>Hello</Col>
+        <Col className='col-5'>No</Col>
+      </div>
+      </div>
+    </Container>
+
     </div>
   );
 }
